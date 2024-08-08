63f88ac1a8d2cbc8b7b663f64dae1121e6ee9ce2
unknown <abdullah.mamun.0110@gmail.com>
2024-08-08 23:10:32 +0600

    remove dist folder

--- dist/app/modules/cloudinary/cloudinary.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.uploadImageRoutes = void 0;
-const express_1 = require("express");
-const cloudinary_controller_1 = require("./cloudinary.controller");
-const router = (0, express_1.Router)();
-router.post('/upload', cloudinary_controller_1.cloudinaryController.uploadImage);
-exports.uploadImageRoutes = router;
